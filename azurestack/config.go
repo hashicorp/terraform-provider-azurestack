608c07587fb61532007b0dc98248203f9c91401d
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2018-04-11 16:48:24 -0500

    Add clients to configuration

--- azurestack/config.go
+++ azurestack/config.go
@@ -1,5 +1,15 @@
 package azurestack
 
+import (
+	hybridcompute "github.com/Azure/azure-sdk-for-go/profiles/2017-03-09/compute/mgmt/compute"
+	"github.com/Azure/azure-sdk-for-go/profiles/2017-03-09/network/mgmt/network"
+	"github.com/Azure/azure-sdk-for-go/profiles/2017-03-09/storage/mgmt/storage"
+)
+
 // ArmClient contains the handles to all the specific Azure Resource Manager
 // resource classes' respective clients.
-type ArmClient struct{}
+type ArmClient struct {
+	vmClient      hybridcompute.VirtualMachinesClient
+	networkClient network.VirtualNetworksClient
+	storageClient storage.AccountsClient
+}

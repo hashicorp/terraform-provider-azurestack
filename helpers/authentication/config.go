565b108046993595582bcba379eac1be909afb94
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2018-04-16 14:09:37 -0500

    Add configuration helper

---
+++ helpers/authentication/config.go
@@ -0,0 +1,25 @@
+package authentication
+
+import (
+	"github.com/Azure/go-autorest/autorest/adal"
+)
+
+// Config is the configuration structure used to instantiate a
+// new Azure Stack management client.
+type Config struct {
+	ClientID       string
+	SubscriptionID string
+	TenantID       string
+	ARMEndpoint    string
+	Location       string
+	Environment    string
+
+	// Service Principal Auth
+	ClientSecret string
+
+	// Bearer Auth
+	AccessToken  *adal.Token
+	IsCloudShell bool
+	UseMsi       bool
+	MsiEndpoint  string
+}

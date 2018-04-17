89b3cc0cc659d58888c5a055e9041fd4cff13ef9
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2018-04-17 15:23:41 -0500

    Remove location

--- azurestack/provider.go
+++ azurestack/provider.go
@@ -1,9 +1,14 @@
 package azurestack
 
 import (
+	"crypto/sha1"
+	"encoding/base64"
+	"encoding/hex"
+	"strings"
+
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/terraform"
-	"github.com/terraform-providers/terraform-provider-azurestack/helpers/authentication"
+	"github.com/terraform-providers/terraform-provider-azurestack/azurestack/helpers/authentication"
 )
 
 func Provider() terraform.ResourceProvider {
@@ -29,11 +34,6 @@ func Provider() terraform.ResourceProvider {
 				Required:    true,
 				DefaultFunc: schema.EnvDefaultFunc("AZURE_CLIENT_SECRET", ""),
 			},
-			"location": {
-				Type:        schema.TypeString,
-				Required:    true,
-				DefaultFunc: schema.EnvDefaultFunc("AZURE_LOCATION", ""),
-			},
 			"arm_endpoint": {
 				Type:        schema.TypeString,
 				Required:    true,
@@ -42,8 +42,10 @@ func Provider() terraform.ResourceProvider {
 		},
 
 		DataSourcesMap: map[string]*schema.Resource{},
-		ResourcesMap:   map[string]*schema.Resource{},
-		ConfigureFunc:  providerConfigureClient,
+		ResourcesMap: map[string]*schema.Resource{
+			"azurestack_resource_group": resourceAzureStackResourceGroup(),
+		},
+		ConfigureFunc: providerConfigureClient,
 	}
 }
 
@@ -53,7 +55,6 @@ func providerConfigureClient(d *schema.ResourceData) (interface{}, error) {
 		ClientID:       d.Get("client_id").(string),
 		ClientSecret:   d.Get("client_secret").(string),
 		TenantID:       d.Get("tenant_id").(string),
-		Location:       d.Get("location").(string),
 		ARMEndpoint:    d.Get("arm_endpoint").(string),
 	}
 
@@ -63,4 +64,55 @@ func providerConfigureClient(d *schema.ResourceData) (interface{}, error) {
 	}
 
 	return client, nil
+}
+
+// Resource group names can be capitalised, but we store them in lowercase.
+// Use a custom diff function to avoid creation of new resources.
+func resourceAzurermResourceGroupNameDiffSuppress(k, old, new string, d *schema.ResourceData) bool {
+	return strings.ToLower(old) == strings.ToLower(new)
+}
+
+// ignoreCaseDiffSuppressFunc is a DiffSuppressFunc from helper/schema that is
+// used to ignore any case-changes in a return value.
+func ignoreCaseDiffSuppressFunc(k, old, new string, d *schema.ResourceData) bool {
+	return strings.ToLower(old) == strings.ToLower(new)
+}
+
+// ignoreCaseStateFunc is a StateFunc from helper/schema that converts the
+// supplied value to lower before saving to state for consistency.
+func ignoreCaseStateFunc(val interface{}) string {
+	return strings.ToLower(val.(string))
+}
+
+func userDataDiffSuppressFunc(k, old, new string, d *schema.ResourceData) bool {
+	oldValue := userDataStateFunc(old)
+	return oldValue == new
+}
+
+func userDataStateFunc(v interface{}) string {
+	switch s := v.(type) {
+	case string:
+		s = base64Encode(s)
+		hash := sha1.Sum([]byte(s))
+		return hex.EncodeToString(hash[:])
+	default:
+		return ""
+	}
+}
+
+// base64Encode encodes data if the input isn't already encoded using
+// base64.StdEncoding.EncodeToString. If the input is already base64 encoded,
+// return the original input unchanged.
+func base64Encode(data string) string {
+	// Check whether the data is already Base64 encoded; don't double-encode
+	if isBase64Encoded(data) {
+		return data
+	}
+	// data has not been encoded encode and return
+	return base64.StdEncoding.EncodeToString([]byte(data))
+}
+
+func isBase64Encoded(data string) bool {
+	_, err := base64.StdEncoding.DecodeString(data)
+	return err == nil
 }
--- azurestack/provider_test.go
+++ azurestack/provider_test.go
@@ -1,6 +1,7 @@
 package azurestack
 
 import (
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform/helper/schema"
@@ -11,13 +12,11 @@ var testAccProviders map[string]terraform.ResourceProvider
 
 var testAccProvider *schema.Provider
 
-var oapi bool
-
 func init() {
 	testAccProvider = Provider().(*schema.Provider)
 
 	testAccProviders = map[string]terraform.ResourceProvider{
-		"azure_stack": testAccProvider,
+		"azurestack": testAccProvider,
 	}
 
 }
@@ -33,4 +32,23 @@ func TestProvider_impl(t *testing.T) {
 }
 
 func testAccPreCheck(t *testing.T) {
+	variables := []string{
+		"AZURE_SUBSCRIPTION_ID",
+		"AZURE_TENANT_ID",
+		"AZURE_CLIENT_ID",
+		"AZURE_CLIENT_SECRET",
+		"AZURE_ARM_ENDPOINT",
+		"ARM_TEST_LOCATION",
+	}
+
+	for _, variable := range variables {
+		value := os.Getenv(variable)
+		if value == "" {
+			t.Fatalf("`%s` must be set for acceptance tests!", variable)
+		}
+	}
+}
+
+func testLocation() string {
+	return os.Getenv("AZURE_TEST_LOCATION")
 }

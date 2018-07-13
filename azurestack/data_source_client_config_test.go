bf3d33f1f9d3cbeb7d432adc931a20ceb66e8944
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2018-07-13 14:08:31 -0500

    Change from AzureRM to AzureStack
    - Removing Public API references

--- azurestack/data_source_client_config_test.go
+++ azurestack/data_source_client_config_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/hashicorp/terraform/terraform"
 )
 
-func TestAccDataSourceAzureRMClientConfig_basic(t *testing.T) {
+func TestAccDataSourceAzureStackClientConfig_basic(t *testing.T) {
 	dataSourceName := "data.azurestack_client_config.current"
 	clientId := os.Getenv("ARM_CLIENT_ID")
 	tenantId := os.Getenv("ARM_TENANT_ID")
@@ -22,11 +22,11 @@ func TestAccDataSourceAzureRMClientConfig_basic(t *testing.T) {
 			{
 				Config: testAccCheckArmClientConfig_basic,
 				Check: resource.ComposeTestCheckFunc(
-					testAzureRMClientConfigAttr(dataSourceName, "client_id", clientId),
-					testAzureRMClientConfigAttr(dataSourceName, "tenant_id", tenantId),
-					testAzureRMClientConfigAttr(dataSourceName, "subscription_id", subscriptionId),
-					testAzureRMClientConfigGUIDAttr(dataSourceName, "service_principal_application_id"),
-					testAzureRMClientConfigGUIDAttr(dataSourceName, "service_principal_object_id"),
+					testAzureStackClientConfigAttr(dataSourceName, "client_id", clientId),
+					testAzureStackClientConfigAttr(dataSourceName, "tenant_id", tenantId),
+					testAzureStackClientConfigAttr(dataSourceName, "subscription_id", subscriptionId),
+					testAzureStackClientConfigGUIDAttr(dataSourceName, "service_principal_application_id"),
+					testAzureStackClientConfigGUIDAttr(dataSourceName, "service_principal_object_id"),
 				),
 			},
 		},
@@ -35,7 +35,7 @@ func TestAccDataSourceAzureRMClientConfig_basic(t *testing.T) {
 
 // Wraps resource.TestCheckResourceAttr to prevent leaking values to console
 // in case of mismatch
-func testAzureRMClientConfigAttr(name, key, value string) resource.TestCheckFunc {
+func testAzureStackClientConfigAttr(name, key, value string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		err := resource.TestCheckResourceAttr(name, key, value)(s)
 		if err != nil {
@@ -47,7 +47,7 @@ func testAzureRMClientConfigAttr(name, key, value string) resource.TestCheckFunc
 	}
 }
 
-func testAzureRMClientConfigGUIDAttr(name, key string) resource.TestCheckFunc {
+func testAzureStackClientConfigGUIDAttr(name, key string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		r, err := regexp.Compile("^[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12}$")
 		if err != nil {

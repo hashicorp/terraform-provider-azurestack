c8612e33fe6122841fb12a1e325c036b8f6470be
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2018-08-02 16:27:05 -0500

    Add loadbalancer backend address pool to provider

---
+++ azurestack/resource_arm_loadbalancer_backend_address_pool_test.go
@@ -0,0 +1,236 @@
+package azurestack
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2015-06-15/network"
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccAzureStackLoadBalancerBackEndAddressPool_basic(t *testing.T) {
+	var lb network.LoadBalancer
+	ri := acctest.RandInt()
+	addressPoolName := fmt.Sprintf("%d-address-pool", ri)
+
+	subscriptionID := os.Getenv("ARM_SUBSCRIPTION_ID")
+	backendAddressPoolId := fmt.Sprintf(
+		"/subscriptions/%s/resourceGroups/acctestRG-%d/providers/Microsoft.Network/loadBalancers/arm-test-loadbalancer-%d/backendAddressPools/%s",
+		subscriptionID, ri, ri, addressPoolName)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureStackLoadBalancerDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAzureStackLoadBalancerBackEndAddressPool_basic(ri, addressPoolName, testLocation()),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureStackLoadBalancerExists("azurestack_lb.test", &lb),
+					testCheckAzureStackLoadBalancerBackEndAddressPoolExists(addressPoolName, &lb),
+					resource.TestCheckResourceAttr(
+						"azurestack_lb_backend_address_pool.test", "id", backendAddressPoolId),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAzureStackLoadBalancerBackEndAddressPool_removal(t *testing.T) {
+	var lb network.LoadBalancer
+	ri := acctest.RandInt()
+	addressPoolName := fmt.Sprintf("%d-address-pool", ri)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureStackLoadBalancerDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAzureStackLoadBalancerBackEndAddressPool_removal(ri, testLocation()),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureStackLoadBalancerExists("azurestack_lb.test", &lb),
+					testCheckAzureStackLoadBalancerBackEndAddressPoolNotExists(addressPoolName, &lb),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAzureStackLoadBalancerBackEndAddressPool_reapply(t *testing.T) {
+	var lb network.LoadBalancer
+	ri := acctest.RandInt()
+	addressPoolName := fmt.Sprintf("%d-address-pool", ri)
+
+	deleteAddressPoolState := func(s *terraform.State) error {
+		return s.Remove("azurestack_lb_backend_address_pool.test")
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureStackLoadBalancerDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAzureStackLoadBalancerBackEndAddressPool_basic(ri, addressPoolName, testLocation()),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureStackLoadBalancerExists("azurestack_lb.test", &lb),
+					testCheckAzureStackLoadBalancerBackEndAddressPoolExists(addressPoolName, &lb),
+					deleteAddressPoolState,
+				),
+				ExpectNonEmptyPlan: true,
+			},
+			{
+				Config: testAccAzureStackLoadBalancerBackEndAddressPool_basic(ri, addressPoolName, testLocation()),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureStackLoadBalancerExists("azurestack_lb.test", &lb),
+					testCheckAzureStackLoadBalancerBackEndAddressPoolExists(addressPoolName, &lb),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAzureStackLoadBalancerBackEndAddressPool_disappears(t *testing.T) {
+	var lb network.LoadBalancer
+	ri := acctest.RandInt()
+	addressPoolName := fmt.Sprintf("%d-address-pool", ri)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckAzureStackLoadBalancerDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAzureStackLoadBalancerBackEndAddressPool_basic(ri, addressPoolName, testLocation()),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckAzureStackLoadBalancerExists("azurestack_lb.test", &lb),
+					testCheckAzureStackLoadBalancerBackEndAddressPoolExists(addressPoolName, &lb),
+					testCheckAzureStackLoadBalancerBackEndAddressPoolDisappears(addressPoolName, &lb),
+				),
+				ExpectNonEmptyPlan: true,
+			},
+		},
+	})
+}
+
+func testCheckAzureStackLoadBalancerBackEndAddressPoolExists(addressPoolName string, lb *network.LoadBalancer) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		_, _, exists := findLoadBalancerBackEndAddressPoolByName(lb, addressPoolName)
+		if !exists {
+			return fmt.Errorf("A BackEnd Address Pool with name %q cannot be found.", addressPoolName)
+		}
+
+		return nil
+	}
+}
+
+func testCheckAzureStackLoadBalancerBackEndAddressPoolNotExists(addressPoolName string, lb *network.LoadBalancer) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		_, _, exists := findLoadBalancerBackEndAddressPoolByName(lb, addressPoolName)
+		if exists {
+			return fmt.Errorf("A BackEnd Address Pool with name %q has been found.", addressPoolName)
+		}
+
+		return nil
+	}
+}
+
+func testCheckAzureStackLoadBalancerBackEndAddressPoolDisappears(addressPoolName string, lb *network.LoadBalancer) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		client := testAccProvider.Meta().(*ArmClient).loadBalancerClient
+		ctx := testAccProvider.Meta().(*ArmClient).StopContext
+
+		_, i, exists := findLoadBalancerBackEndAddressPoolByName(lb, addressPoolName)
+		if !exists {
+			return fmt.Errorf("A BackEnd Address Pool with name %q cannot be found.", addressPoolName)
+		}
+
+		currentPools := *lb.LoadBalancerPropertiesFormat.BackendAddressPools
+		pools := append(currentPools[:i], currentPools[i+1:]...)
+		lb.LoadBalancerPropertiesFormat.BackendAddressPools = &pools
+
+		id, err := parseAzureResourceID(*lb.ID)
+		if err != nil {
+			return err
+		}
+
+		future, err := client.CreateOrUpdate(ctx, id.ResourceGroup, *lb.Name, *lb)
+		if err != nil {
+			return fmt.Errorf("Error Creating/Updating LoadBalancer %+v", err)
+		}
+
+		err = future.WaitForCompletion(ctx, client.Client)
+		if err != nil {
+			return fmt.Errorf("Error Creating/Updating LoadBalancer %+v", err)
+		}
+
+		_, err = client.Get(ctx, id.ResourceGroup, *lb.Name, "")
+		return err
+	}
+}
+
+func testAccAzureStackLoadBalancerBackEndAddressPool_basic(rInt int, addressPoolName string, location string) string {
+	return fmt.Sprintf(`
+resource "azurestack_resource_group" "test" {
+  name     = "acctestRG-%d"
+  location = "%s"
+}
+
+resource "azurestack_public_ip" "test" {
+  name                         = "test-ip-%d"
+  location                     = "${azurestack_resource_group.test.location}"
+  resource_group_name          = "${azurestack_resource_group.test.name}"
+  public_ip_address_allocation = "static"
+}
+
+resource "azurestack_lb" "test" {
+  name                = "arm-test-loadbalancer-%d"
+  location            = "${azurestack_resource_group.test.location}"
+  resource_group_name = "${azurestack_resource_group.test.name}"
+
+  frontend_ip_configuration {
+    name                 = "one-%d"
+    public_ip_address_id = "${azurestack_public_ip.test.id}"
+  }
+}
+
+resource "azurestack_lb_backend_address_pool" "test" {
+  location            = "${azurestack_resource_group.test.location}"
+  resource_group_name = "${azurestack_resource_group.test.name}"
+  loadbalancer_id     = "${azurestack_lb.test.id}"
+  name                = "%s"
+}
+
+`, rInt, location, rInt, rInt, rInt, addressPoolName)
+}
+
+func testAccAzureStackLoadBalancerBackEndAddressPool_removal(rInt int, location string) string {
+	return fmt.Sprintf(`
+resource "azurestack_resource_group" "test" {
+  name     = "acctestRG-%d"
+  location = "%s"
+}
+
+resource "azurestack_public_ip" "test" {
+  name                         = "test-ip-%d"
+  location                     = "${azurestack_resource_group.test.location}"
+  resource_group_name          = "${azurestack_resource_group.test.name}"
+  public_ip_address_allocation = "static"
+}
+
+resource "azurestack_lb" "test" {
+  name                = "arm-test-loadbalancer-%d"
+  location            = "${azurestack_resource_group.test.location}"
+  resource_group_name = "${azurestack_resource_group.test.name}"
+
+  frontend_ip_configuration {
+    name                 = "one-%d"
+    public_ip_address_id = "${azurestack_public_ip.test.id}"
+  }
+}
+`, rInt, location, rInt, rInt, rInt)
+}

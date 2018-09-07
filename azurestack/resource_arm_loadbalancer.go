8f02d6bdcb10b7273360eb55822cee464c683d76
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2018-09-07 14:48:48 -0500

    For some reason gofmt complained about this file

--- azurestack/resource_arm_loadbalancer.go
+++ azurestack/resource_arm_loadbalancer.go
@@ -10,9 +10,9 @@ import (
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/helper/validation"
-	"github.com/terraform-providers/terraform-provider-azurestack/azurestack/helpers/azure"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/suppress"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/validate"
+	"github.com/terraform-providers/terraform-provider-azurestack/azurestack/helpers/azure"
 	"github.com/terraform-providers/terraform-provider-azurestack/azurestack/utils"
 )
 

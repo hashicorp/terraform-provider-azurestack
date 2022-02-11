fe45eb8c264d2a204b5ab76eee3754f2ca6642f5
Edgar López <edgars.ealo@gmail.com>
2022-02-11 12:10:42 -0700

    feat: added resource linux virtual machine, many files that requires for the resource linux vm

---
+++ internal/services/msi/parse/user_assigned_identity_id.go
@@ -0,0 +1,13 @@
+package parse
+
+import "github.com/hashicorp/terraform-provider-azurestack/internal/services/msi/sdk/2018-11-30/managedidentity"
+
+// these are here primarily to enable migration over
+
+func UserAssignedIdentityID(input string) (*managedidentity.UserAssignedIdentitiesId, error) {
+	return managedidentity.ParseUserAssignedIdentitiesID(input)
+}
+
+func UserAssignedIdentityIDInsensitively(input string) (*managedidentity.UserAssignedIdentitiesId, error) {
+	return managedidentity.ParseUserAssignedIdentitiesIDInsensitively(input)
+}

d3c4db7d9f0aefa3ab58336d785e33517529c427
Edgar López <edgars.ealo@gmail.com>
2022-02-28 13:11:26 -0700

    test: added tests for linux using auth

---
+++ internal/services/compute/linux_virtual_machine_resource_auth_test.go
@@ -0,0 +1,208 @@
+package compute_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-provider-azurestack/internal/tf/acceptance"
+	"github.com/hashicorp/terraform-provider-azurestack/internal/tf/acceptance/check"
+)
+
+func TestAccLinuxVirtualMachine_authPassword(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurestack_linux_virtual_machine", "test")
+	r := LinuxVirtualMachineResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.authPassword(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("admin_password", "delete_os_disk_on_termination"),
+	})
+}
+
+func TestAccLinuxVirtualMachine_authPasswordAndSSH(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurestack_linux_virtual_machine", "test")
+	r := LinuxVirtualMachineResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.authPasswordAndSSH(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("admin_password", "delete_os_disk_on_termination"),
+	})
+}
+
+func TestAccLinuxVirtualMachine_authSSH(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurestack_linux_virtual_machine", "test")
+	r := LinuxVirtualMachineResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.authSSH(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("delete_os_disk_on_termination"),
+	})
+}
+
+func TestAccLinuxVirtualMachine_authSSHMultipleKeys(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurestack_linux_virtual_machine", "test")
+	r := LinuxVirtualMachineResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.authSSHMultiple(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep("delete_os_disk_on_termination"),
+	})
+}
+
+func (r LinuxVirtualMachineResource) authPassword(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurestack_linux_virtual_machine" "test" {
+  name                            = "acctestVM-%d"
+  resource_group_name             = azurestack_resource_group.test.name
+  location                        = azurestack_resource_group.test.location
+  size                            = "Standard_F2"
+  admin_username                  = "adminuser"
+  admin_password                  = "P@$$w0rd1234!"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurestack_network_interface.test.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
+`, r.template(data), data.RandomInteger)
+}
+
+func (r LinuxVirtualMachineResource) authPasswordAndSSH(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurestack_linux_virtual_machine" "test" {
+  name                = "acctestVM-%d"
+  resource_group_name = azurestack_resource_group.test.name
+  location            = azurestack_resource_group.test.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurestack_network_interface.test.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = local.first_public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
+`, r.template(data), data.RandomInteger)
+}
+
+func (r LinuxVirtualMachineResource) authSSH(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurestack_linux_virtual_machine" "test" {
+  name                = "acctestVM-%d"
+  resource_group_name = azurestack_resource_group.test.name
+  location            = azurestack_resource_group.test.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurestack_network_interface.test.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = local.first_public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
+`, r.template(data), data.RandomInteger)
+}
+
+func (r LinuxVirtualMachineResource) authSSHMultiple(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurestack_linux_virtual_machine" "test" {
+  name                = "acctestVM-%d"
+  resource_group_name = azurestack_resource_group.test.name
+  location            = azurestack_resource_group.test.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurestack_network_interface.test.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = local.first_public_key
+  }
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = local.second_public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
+`, r.template(data), data.RandomInteger)
+}

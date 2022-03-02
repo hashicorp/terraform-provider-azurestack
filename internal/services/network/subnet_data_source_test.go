7e03ba44059378fdaff95b4677314c5797acc0c5
Abner Garcia <abner.garcia@digitalonus.com>
2022-03-02 13:49:11 -0700

    updated subnet datasource

--- internal/services/network/subnet_data_source_test.go
+++ internal/services/network/subnet_data_source_test.go
@@ -29,29 +29,6 @@ func TestAccSubnetDataSource_basic(t *testing.T) {
 	})
 }
 
-func TestAccSubnetDataSource_routeTable(t *testing.T) {
-	data := acceptance.BuildTestData(t, "data.azurestack_subnet", "test")
-	r := SubnetDataSource{}
-
-	data.DataSourceTest(t, []acceptance.TestStep{
-		{
-			// since the route table association is a separate resource this forces it
-			Config: r.routeTableDependencies(data),
-		},
-		{
-			Config: r.routeTable(data),
-			Check: acceptance.ComposeTestCheckFunc(
-				check.That(data.ResourceName).Key("name").Exists(),
-				check.That(data.ResourceName).Key("resource_group_name").Exists(),
-				check.That(data.ResourceName).Key("virtual_network_name").Exists(),
-				check.That(data.ResourceName).Key("address_prefix").Exists(),
-				check.That(data.ResourceName).Key("route_table_id").Exists(),
-				check.That(data.ResourceName).Key("network_security_group_id").HasValue(""),
-			),
-		},
-	})
-}
-
 func (r SubnetDataSource) basic(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 %s
@@ -69,49 +46,6 @@ data "azurestack_subnet" "test" {
   resource_group_name  = azurestack_subnet.test.resource_group_name
 }
 `, r.template(data))
-}
-
-func (r SubnetDataSource) routeTableDependencies(data acceptance.TestData) string {
-	return fmt.Sprintf(`
-%s
-
-resource "azurestack_subnet" "test" {
-  name                 = "internal"
-  resource_group_name  = azurestack_resource_group.test.name
-  virtual_network_name = azurestack_virtual_network.test.name
-  address_prefix       = "10.0.0.0/24"
-}
-
-resource "azurestack_route_table" "test" {
-  name                = "acctestrt-%d"
-  location            = azurestack_resource_group.test.location
-  resource_group_name = azurestack_resource_group.test.name
-
-  route {
-    name                   = "first"
-    address_prefix         = "10.100.0.0/14"
-    next_hop_type          = "VirtualAppliance"
-    next_hop_in_ip_address = "10.10.1.1"
-  }
-}
-
-resource "azurestack_subnet_route_table_association" "test" {
-  subnet_id      = azurestack_subnet.test.id
-  route_table_id = azurestack_route_table.test.id
-}
-`, r.template(data), data.RandomInteger)
-}
-
-func (r SubnetDataSource) routeTable(data acceptance.TestData) string {
-	return fmt.Sprintf(`
-%s
-
-data "azurestack_subnet" "test" {
-  name                 = azurestack_subnet.test.name
-  virtual_network_name = azurestack_subnet.test.virtual_network_name
-  resource_group_name  = azurestack_subnet.test.resource_group_name
-}
-`, r.routeTableDependencies(data))
 }
 
 func (SubnetDataSource) template(data acceptance.TestData) string {

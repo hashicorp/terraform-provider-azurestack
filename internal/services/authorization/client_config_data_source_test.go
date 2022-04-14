2be1310ae03ec0410609cb19032d1b89d37a372f
Edgar López <edgars.ealo@gmail.com>
2022-04-14 15:42:19 -0600

    fixes test of authorization

--- internal/services/authorization/client_config_data_source_test.go
+++ internal/services/authorization/client_config_data_source_test.go
@@ -25,7 +25,7 @@ func TestAccClientConfigDataSource_basic(t *testing.T) {
 				check.That(data.ResourceName).Key("client_id").HasValue(clientId),
 				check.That(data.ResourceName).Key("tenant_id").HasValue(tenantId),
 				check.That(data.ResourceName).Key("subscription_id").HasValue(subscriptionId),
-				check.That(data.ResourceName).Key("object_id").MatchesRegex(objectIdRegex),
+				check.That(data.ResourceName).Key("service_principal_object_id").MatchesRegex(objectIdRegex),
 			),
 		},
 	})

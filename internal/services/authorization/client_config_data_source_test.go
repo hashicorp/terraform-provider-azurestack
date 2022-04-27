81e524eb1e04336733f859f6440cf099da23d767
Edgar López <edgars.ealo@gmail.com>
2022-04-27 12:54:20 -0700

    Revert "Fix auth objectid (#184)"
    This reverts commit 6017e4774912d54c57c944b063286557d093716e.

--- internal/services/authorization/client_config_data_source_test.go
+++ internal/services/authorization/client_config_data_source_test.go
@@ -1,15 +1,12 @@
 package authorization_test
 
 import (
-	"fmt"
 	"os"
 	"regexp"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-provider-azurestack/internal/tf/acceptance"
 	"github.com/hashicorp/terraform-provider-azurestack/internal/tf/acceptance/check"
-	"github.com/hashicorp/terraform-provider-azurestack/internal/tf/pluginsdk"
 )
 
 type ClientConfigDataSource struct{}
@@ -19,6 +16,7 @@ func TestAccClientConfigDataSource_basic(t *testing.T) {
 	clientId := os.Getenv("ARM_CLIENT_ID")
 	tenantId := os.Getenv("ARM_TENANT_ID")
 	subscriptionId := os.Getenv("ARM_SUBSCRIPTION_ID")
+	objectIdRegex := regexp.MustCompile("^[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12}$")
 
 	data.DataSourceTest(t, []acceptance.TestStep{
 		{
@@ -27,35 +25,15 @@ func TestAccClientConfigDataSource_basic(t *testing.T) {
 				check.That(data.ResourceName).Key("client_id").HasValue(clientId),
 				check.That(data.ResourceName).Key("tenant_id").HasValue(tenantId),
 				check.That(data.ResourceName).Key("subscription_id").HasValue(subscriptionId),
-				testAccCheckRegexSIDs("data.azurestack_client_config.current"),
+				check.That(data.ResourceName).Key("service_principal_object_id").MatchesRegex(objectIdRegex),
 			),
 		},
 	})
 }
 
-func testAccCheckRegexSIDs(resourceName string) pluginsdk.TestCheckFunc {
-	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[resourceName]
-		if !ok {
-			return fmt.Errorf("not found: %s", resourceName)
-		}
-
-		objectIdRegex := regexp.MustCompile("^[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12}$")
-		adfsID := regexp.MustCompile(`^S-\d-(\d+-){1,14}\d+$`)
-
-		if !objectIdRegex.MatchString(rs.Primary.Attributes["object_id"]) {
-			// For ADFS validation
-			if !adfsID.MatchString(rs.Primary.Attributes["object_id"]) {
-				return fmt.Errorf("object_id didn't match %v or %v, got %v", objectIdRegex, adfsID, rs.Primary.Attributes["object_id"])
-			}
-		}
-
-		return nil
-	}
-}
-
 func (d ClientConfigDataSource) basic() string {
 	return `
-data "azurestack_client_config" "current" {}
+data "azurestack_client_config" "current" {
+}
 `
 }
